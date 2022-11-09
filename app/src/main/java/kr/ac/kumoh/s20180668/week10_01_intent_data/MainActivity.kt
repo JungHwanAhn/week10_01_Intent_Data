b4a053b0a83b05279194fa2b5475f15144f354db
JungHwanAhn <wjdghks1831@naver.com>
2022-11-09 14:37:50 +0900

    📝 버튼 추가

--- app/src/main/java/kr/ac/kumoh/s20180668/week10_01_intent_data/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20180668/week10_01_intent_data/MainActivity.kt
@@ -3,15 +3,61 @@ package kr.ac.kumoh.s20180668.week10_01_intent_data
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import kr.ac.kumoh.s20180668.week10_01_intent_data.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    View.OnClickListener {
+
+    companion object {
+        const val keyName = "image"
+    }
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var launcher: ActivityResultLauncher<Intent>
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, ImageActivity::class.java)
+        val value = when(v?.id) {
+            binding.btnSoju.id -> "soju"
+            binding.btnSea.id -> "sea"
+            else -> return
+        }
+        intent.putExtra(keyName, value)
+        launcher.launch(intent)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnSoju.setOnClickListener(this)
+        binding.btnSea.setOnClickListener(this)
+
+        launcher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode != RESULT_OK)
+                return@registerForActivityResult
+
+            val result = it.data?.getIntExtra(ImageActivity.resultName,
+            ImageActivity.NONE)
+
+            val str = when(result) {
+                ImageActivity.LIKE -> "좋아요"
+                ImageActivity.DISLIKE -> "싫어요"
+                else -> ""
+            }
+            val image = it.data?.getStringExtra(ImageActivity.imageName)
+            when(image) {
+                "soju" -> binding.btnSoju.text = "감성 ($str)"
+                "sea" -> binding.btnSea.text = "바다 ($str)"
+            }
+        }
+
+        /*
         binding.btnSoju.setOnClickListener {
             val intent = Intent(this, ImageActivity::class.java)
             intent.putExtra("image", "soju")
@@ -23,5 +69,6 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("image", "sea")
             startActivity(intent)
         }
+        */
     }
 }
